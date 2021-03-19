a5c7a9992c299ad48af0d0228aa0e5e885a8c813
Leo <portelanceleo@gmail.com>
2021-03-19 15:34:36 -0400

    third commit

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 
 textIn  = document.getElementById("input-textbox");
 textOut = document.getElementById("output-textbox");
-inData = document.getElementById("data").childNodes;
-majeurArr = []
-minorArr = []
+inData = document.getElementById("data")
+majeurArr = [];
+mineurArr = [];
+
+
+
 
 
 function process(input) {
@@ -22,54 +25,54 @@ function selectGame(gameId){
   switch(gameId){
     case 1:
       textOut.value += "1" + "\n";
-      //curGame = new Game1();
+      curGame = new Game1();
       break;
     case 2:
       textOut.value += "2" + "\n";
-      //curGame = new Game2();
+      curGame = new Game2();
       break;
     case 3:
       textOut.value += "3" + "\n";
-      //curGame = new Game3();
+      curGame = new Game3();
       break;
     case 4:
       textOut.value += "4" + "\n";
-      //curGame = new Game4();
+      curGame = new Game4();
       break;
-
   }
+  curGame.getNextQuestion()
 }
-function test(){
-  var txt = "";
-
-  for (var i = 0; i < inData.length; i++) {
-      if(inData[i].id == "majeur"){
-        majData = inData[i].childNodes[1].childNodes
-        textOut.value += "majeur" + "\n"
+function getData(){
+  inNodes = inData.childNodes
+  for (var i = 0; i < inNodes .length; i++) {
+      if(inNodes[i].id == "majeur"){
+        majData = inNodes[i].childNodes[1].childNodes
         id = majData[1].innerHTML
-        textOut.value += id + "\n"
         majeur = new Majeur(id,majData)
         majeurArr.push(majeur)
       }
-      if(inData[i].id == "mineur"){
-        minData = inData[i].childNodes[1].childNodes
-        textOut.value += "minor" + "\n"
+      if(inNodes[i].id == "mineur"){
+        minData = inNodes[i].childNodes[1].childNodes
         id = minData[1].innerHTML
-        textOut.value += id + "\n"
         naturalData = minData[3].childNodes[1].childNodes;
         harmoniqueData = minData[5].childNodes[1].childNodes
         mineur = new Mineur(id,naturalData,harmoniqueData)
-        minorArr.push
+        mineurArr.push(mineur)
       }
 
+
   }
+  inData.innerHTML = ""
 
 }
+function getRandMajeur(){
+  return majeurArr[Math.floor(Math.random() * majeurArr.length)]
+}
 
-/*<ul id = "majeur">
-  <li id = "id"><li>
-  <li
-</ul>*/
+function getRandMineur(){
+  return mineurArr[Math.floor(Math.random() * mineurArr.length)]
+}
+
 class Mode{
   constructor(data){
     for (var i = 0; i < data.length; i++){
@@ -77,26 +80,35 @@ class Mode{
       {
         case "tonique":
           this.tonique = data[i].innerHTML;
-          textOut.value += data[i].innerHTML + "\n"
+
           break;
         case "mediante":
           this.mediante = data[i].innerHTML;
-          textOut.value += data[i].innerHTML + "\n"
+
           break;
         case "dominante":
           this.dominante = data[i].innerHTML;
-          textOut.value += data[i].innerHTML + "\n"
+
           break;
         case "sensible":
           this.sensible = data[i].innerHTML;
-          textOut.value += data[i].innerHTML + "\n"
+
           break;
         case "echelle":
           this.echelle = data[i].innerHTML;
-          textOut.value += data[i].innerHTML + "\n"
+
           break;
       }
     }
+  }
+  compare(mode){
+    isToniqueSame = this.tonique == mode.tonique;
+    isSensibleSame = this.sensible == mode.sensible;
+    isDominanteSame = this.dominante == mode.dominante;
+    isEchelleSame = this.echelle == mode.echelle;
+    isMedianteSame =this.mediante == mode.mediante;
+    return isMedianteSame && isEchelleSame && isDominanteSame && isSensibleSame && isToniqueSame
+
   }
 }
 
@@ -108,6 +120,11 @@ class Majeur{
       this.id = id;
       this.mode = new Mode(data);
     }
+    compare(majeur){
+      if(this.id == majeur.id){
+        return this.mode.compare(majeur.mode)
+      }
+    }
 }
 
 class Mineur{
@@ -116,18 +133,61 @@ class Mineur{
     this.naturel = new Mode(naturel);
     this.harmonique = new Mode(harmonique);
   }
+  compare(mineur){
+    if(this.id == mineur.id){
+      return this.natural.compare(mineur.natural) && this.harmonique.compare(mineur.harmonique)
+    }
 
+  }
 }
-
-/*
 class Game {
-    constructor(){}
-    checkAnswer(answer){}
+    constructor(){
+      this.testCount = 0;
+      this.score = 0;
+    }
+    checkAnswer(){
+      var answer = textIn.value
+      console.log(answer)
+      if(this.answer == answer){
+        textOut.value += "Bonne reponse\n";
+        this.score += 1;
+        console.log("yyuea")
+      }
+      else{
+        textOut.value += "Mauvaise reponse, la bonne reponse est {}\n".replace("{}",this.answer)
+        console.log("yfds")
+      }
+      textIn.value = ""
+      this.getNextQuestion()
+
+    }
     getNextQuestion(){}
 }
 
-class Game1 extends Game{}
-class Game2 extends Game{}
-class Game3 extends Game{}
-class Game4 extends Game{}
-*/
+class Game1 extends Game{
+  constructor(){
+    super()
+  }
+  getNextQuestion(){
+    this.testCount += 1;
+    this.curData = getRandMajeur();
+    this.answer = this.curData.mode.mediante;
+    var questionTemplate = "\nQuel est la mediante de la clé majeur {} ?\n".replace("{}",this.curData.id);
+    textOut.value += questionTemplate + "\n";
+  }
+}
+class Game2 extends Game{
+  constructor(){
+    super()
+  }
+}
+class Game3 extends Game{
+  constructor(){
+    super()
+  }
+}
+class Game4 extends Game{
+  constructor(){
+    super()
+  }
+}
